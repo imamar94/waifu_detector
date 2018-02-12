14168902d6ef66302d3a0745ae88a12e9550df7e
Imam Abdul Rahman <imam.arrr@gmail.com>
2018-02-12 22:48:12 +0700

    release waifu detector dataset to public

---
+++ generate_tfrecord.sh
@@ -0,0 +1,2 @@
+python generate_tfrecord.py --images_path=train/images --csv_input=train/train.csv  --output_path=data/train.record
+python generate_tfrecord.py --images_path=test/images --csv_input=test/test.csv  --output_path=data/test.record
